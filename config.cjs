d04c6641160f419fb9bc08b5754c5fad6dab1509
Raheem-cm <nyoniraheem@gmail.com>
2025-07-02 01:28:49 +0300

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -4,7 +4,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "KAISEN~f4RkTICJ#FpRjM-TRz4RTGsaSuF_fQW6rJbmz_xjjGch58gf5lVo",
+  SESSION_ID: process.env.SESSION_ID || "RAHEEM-XMD-2>>>PoZXHABR#avdbUBpb_iJdpAgpgxTif2tKWE4y30tADqir93bdUqQ",
   PREFIX: process.env.PREFIX || '+',
   AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true, 
   AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY !== undefined ? process.env.AUTO_STATUS_REPLY === 'true' : true,
